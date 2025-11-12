d8ffa2b25a13250a2a9bab54f477cbce589abef9
Salihkilicc <118219897+Salihkilicc@users.noreply.github.com>
2025-11-12 17:46:01 +0300

    google

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from 'expo-router';
 
+
+import * as WebBrowser from "expo-web-browser";
+
+
+WebBrowser.maybeCompleteAuthSession();
+
 export default function AuthLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
